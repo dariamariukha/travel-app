be68a9a7e7f1be66c9d3d12d5a061d1e0e806ba3
unknown <mariukha.daria@student.sumdu.edu.ua>
2025-11-08 12:22:22 +0200

    step-03: add Navbar, Footer, and DestinationCard components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Destinations } from './pages/destinations/destinations';
+import { About } from './pages/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'destinations', component: Destinations},
+  { path: 'about', component: About },
+  { path: '**', redirectTo: 'home' }
+];
+
+console.log('✅ ROUTES LOADED', routes);
